5eb6baed9a986f224e2c631d83c227c4b84b7504
Chris Pruitt <chris.pruitt15@gmail.com>
2020-04-27 07:24:59 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pythena',
-    version='v1.4.2',
+    version='v1.5.0',
     license='Mozilla Public License Version 2.0',
     author='chris.pruitt',
     url='https://github.com/chrispruitt/pythena',
